b842561ecf719ad17bbb3a9828919a8367097729
morganhamm <morganohamm@gmail.com>
2022-09-02 16:03:03 -0700

    created ReadList object

--- fiber_views/utils.py
+++ fiber_views/utils.py
@@ -6,8 +6,6 @@ Created on Tue Aug 30 16:05:34 2022
 @author: morgan
 """
 
-# TODO: make strand specific.
-# TODO: bed to df function
 
 import numpy as np
 import pandas as pd
@@ -20,7 +18,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 from Bio.Seq import Seq
-import re
+# import re
 import sys
 
 
@@ -28,6 +26,104 @@ CPG_MODS = [("C", 0, "m")]
 M6A_MODS = [("A", 0, "a"), ("T", 1, "a")]
 D_TYPE = np.int64
 
+
+# =============================================================================
+# CLASSES
+# =============================================================================
+
+class ReadList(list):
+    """
+    A simple list of pysam.libcalignedsegment.PileupRead objects, plus methods
+    useful for constructing anndata elements from the read objects. also tracks
+    strand info of the genomic query position
+    """
+    def __init__(self, normal_list=[], strand="+"):
+        super().__init__()
+        self.strand = strand
+        self.extend(normal_list)
+    def get_reads(self, alignment_file, ref_pos):
+        """Example: reads=ReadList().get_reads(bamfile, ('chr3', 200000, '+'))
+        """
+        pileup_iter = alignment_file.pileup(ref_pos[0], ref_pos[1], ref_pos[1] +1 )   
+        for pileup_column in pileup_iter:
+            if pileup_column.pos == ref_pos[1]:
+                reads = [read for read in pileup_column.pileups 
+                         if not read.query_position is None]
+                break
+            elif pileup_column.pos > ref_pos[1]:
+                break
+        self.clear()
+        self.strand = ref_pos[2]
+        self.extend(reads)
+        return(self)        
+    def filter_by_window(self, window_offset, inplace=False):
+        """remove reads that don't fully span a window of +/- window_offset.
+            if inplace == True: this instance is modified, else: a new ReadList 
+            object is returned"""
+        list_out = [read for read in self if read.query_position - window_offset >= 0 and 
+                read.query_position + window_offset <= read.alignment.query_length]
+        if inplace:
+            self.clear()
+            self.extend(list_out)
+            return(None)
+        else:
+            return(ReadList(list_out, self.strand))
+    def build_seq_array(self, window_offset, strand=None):
+        # create a byte array of the sequences. 
+        # warning, filter reads first
+        if strand is None:
+            strand = self.strand
+        char_array = np.empty((len(self), 2*window_offset), dtype="S1")
+        for i, read in enumerate(self):
+            center_pos = read.query_position
+            seq = read.alignment.query_sequence[center_pos - window_offset : 
+                                                center_pos + window_offset]
+            if strand == "-":
+                seq = str(Seq(seq).reverse_complement())
+            char_array[i, :] = np.frombuffer(seq.encode('UTF-8'), dtype="S1")
+        return(char_array)
+    def build_mod_array(self, window_offset, mod_type=M6A_MODS, strand=None, 
+                        sparse=True, score_cutoff=200):
+        if strand is None:
+            strand = self.strand
+        I = []
+        J = []
+        none_count = 0
+        for i, read in enumerate(self):
+            mods = get_strand_correct_mods(read, mod_type, centered=True, 
+                                           score_cutoff=score_cutoff)
+            if mods is None:
+                none_count += 1
+                continue
+            mods = mods + window_offset
+            if strand == "-":
+                mods = (2*window_offset-1) - mods - 1 * (mod_type == CPG_MODS)
+            mods = [mod for mod in mods if mod >= 0 and mod <= 2*window_offset-1]
+            for mod in mods:
+                I.append(i)
+                J.append(mod)
+        V = np.ones((len(J)), dtype=bool)
+        mod_mtx = coo_matrix((V, (I, J)), shape=(len(self), 2*window_offset))
+        if sparse == False:
+            mod_mtx = mod_mtx.toarray()       
+        return(mod_mtx)
+    def build_anno_df(self, anno_series):
+        row_data_dict = anno_series.to_dict()
+        row_data_dict['read_name'] = [read.alignment.query_name for read in self]
+        row_data_dict['read_length'] = [read.alignment.query_length for read in self]
+        row_data_dict['read_flag'] = [read.alignment.flag for read in self]
+        return(pd.DataFrame(row_data_dict))
+    def print_aligned_centers(self, offset=5):
+        # test function to make sure reads are aligning correctly
+        for i, read in enumerate(self):
+            center_pos = read.query_position
+            alignment = read.alignment
+            seq = Seq(alignment.query_sequence)
+            print(str(i)+ ":  " + seq[center_pos-offset:center_pos] + ">" + 
+                  seq[center_pos:center_pos+offset+1])
+        
+
+    
     
 # =============================================================================
 # FUNCTIONS
@@ -233,7 +329,7 @@ def collapse_anndata_by_obs(adata, obs_col_name='site_name', cols_to_keep=[]):
         cpgs.append(np.sum(a_subset.layers['cpg'], axis=0))
         m6as.append(np.sum(a_subset.layers['m6a'], axis=0))
         # take the first row of subset.obs as new obs row
-        new_obs_row = a_subset.obs[cols_to_keep].iloc[1].copy()
+        new_obs_row = a_subset.obs[cols_to_keep].iloc[0].copy()
         new_obs_row['n_seqs'] = a_subset.shape[0]
         new_obs_rows.append(new_obs_row)
     new_adata = ad.AnnData(
@@ -372,6 +468,11 @@ bed_data.query('not chrom in ["chrC", "chrM"]')
 
 # reads = get_reads_at_center_pos(bamfile, GenomicPosition().from_str("chr3:2015001"))
 reads = get_reads_at_center_pos(bamfile, ("chr3", 2015001, '+'))
+
+
+reads = ReadList().get_reads(bamfile, ("chr3", 2015001, '+'))
+
+
 
 print_aligned_reads(reads, offset=50)
 
