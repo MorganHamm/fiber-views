7636a5ed10471d0763830c27445fb72348e4d462
morganhamm <morganohamm@gmail.com>
2022-09-02 16:33:18 -0700

    cleaned up redundant functions

--- fiber_views/utils.py
+++ fiber_views/utils.py
@@ -148,32 +148,6 @@ def get_mod_pos_from_rec(rec, mods=M6A_MODS, score_cutoff=200):
     return mod_positions
 
 
-def get_reads_at_center_pos(alignment_file, ref_pos):
-    # ref_pos in form of (seqid, pos, strand) ie. ('chr3', 5000, '-')
-    # returns list of pysam.libcalignedsegment.PileupRead objects 
-    pileup_iter = alignment_file.pileup(ref_pos[0], ref_pos[1], ref_pos[1] +1 )   
-    for pileup_column in pileup_iter:
-        if pileup_column.pos == ref_pos[1]:
-            reads = [read for read in pileup_column.pileups 
-                     if not read.query_position is None]
-            return(reads)
-        elif pileup_column.pos > ref_pos[1]:
-            return(None)
-    return(None)
-
-def print_aligned_reads(reads, offset=5):
-    # test function to make sure reads are aligning correctly
-    for i, read in enumerate(reads):
-        center_pos = read.query_position
-        alignment = read.alignment
-        if alignment.is_reverse:
-            # alignment.query_sequence is on forward genomic strand
-            seq = Seq(alignment.query_sequence)# .reverse_complement()
-        else:
-            seq = Seq(alignment.query_sequence)
-        print(str(i)+ ":  " + seq[center_pos-offset:center_pos] + ">" + 
-              seq[center_pos:center_pos+offset+1])
-        
 
 def print_mod_contexts(read, mod_positions, offset=5, use_strand=True):
     # test function make sure mods are aligned correctly
@@ -205,55 +179,6 @@ def get_strand_correct_mods(read, mod_type=M6A_MODS, centered=False, score_cutof
     if centered:
         mods = mods - read.query_position
     return(mods)
-  
-def filter_reads_by_window(reads, window_offset):
-    # filter reads to only those that fill the window around the center position
-    if reads is None:
-        return(None)
-    return([read for read in reads if read.query_position - window_offset >= 0 and 
-     read.query_position + window_offset <= read.alignment.query_length])
-  
-def build_seq_array(reads, window_offset, strand="+"):
-    # create a byte array of the sequences. 
-    # warning, filter reads first
-    char_array = np.empty((len(reads), 2*window_offset), dtype="S1")
-    for i, read in enumerate(reads):
-        center_pos = read.query_position
-        seq = read.alignment.query_sequence[center_pos - window_offset : center_pos + window_offset]
-        if strand == "-":
-            seq = str(Seq(seq).reverse_complement())
-        char_array[i, :] = np.frombuffer(seq.encode('UTF-8'), dtype="S1")
-    return(char_array)
-
-
-def build_mod_array(reads, window_offset, mod_type=M6A_MODS, strand="+", sparse=True, score_cutoff=200):
-    I = []
-    J = []
-    none_count = 0
-    for i, read in enumerate(reads):
-        mods = get_strand_correct_mods(read, mod_type, centered=True, score_cutoff=score_cutoff)
-        if mods is None:
-            none_count += 1
-            continue
-        mods = mods + window_offset
-        if strand == "-":
-            mods = (2*window_offset-1) - mods - 1 * (mod_type == CPG_MODS)
-        mods = [mod for mod in mods if mod >= 0 and mod <= 2*window_offset-1]
-        for mod in mods:
-            I.append(i)
-            J.append(mod)
-    V = np.ones((len(J)), dtype=bool)
-    mod_mtx = coo_matrix((V, (I, J)), shape=(len(reads), 2*window_offset))
-    if sparse == False:
-        mod_mtx = mod_mtx.toarray()       
-    return(mod_mtx)
-
-def build_row_anno_from_reads(reads, anno_series):
-    row_data_dict = anno_series.to_dict()
-    row_data_dict['read_name'] = [read.alignment.query_name for read in reads]
-    row_data_dict['read_length'] = [read.alignment.query_length for read in reads]
-    row_data_dict['read_flag'] = [read.alignment.flag for read in reads]
-    return(pd.DataFrame(row_data_dict))
 
 
 def build_anndata_from_df(alignment_file, df, window_offset=1000):
@@ -262,22 +187,19 @@ def build_anndata_from_df(alignment_file, df, window_offset=1000):
     m6a_mtx_list = []
     cpg_mtx_list = []
     for i, row in df.iterrows():
-        reads = get_reads_at_center_pos(alignment_file, 
-                                        ref_pos=(row.seqid, row.pos, row.strand))
-        reads = filter_reads_by_window(reads, window_offset)
-        if reads is None:
+        reads = ReadList().get_reads(alignment_file, 
+                                     ref_pos=(row.seqid, row.pos, row.strand))
+        reads.filter_by_window(window_offset, inplace=True)
+        if len(reads) == 0:
             continue
-        row_anno_df_list.append(build_row_anno_from_reads(reads, row))
-        seq_mtx_list.append(build_seq_array(reads, window_offset, 
-                                            strand=row.loc['strand']))
-        m6a_mtx_list.append(build_mod_array(reads, window_offset, 
-                                            mod_type=M6A_MODS, sparse=True, 
-                                            score_cutoff=220,
-                                            strand=row.loc['strand']))
-        cpg_mtx_list.append(build_mod_array(reads, window_offset, 
-                                            mod_type=CPG_MODS, sparse=True, 
-                                            score_cutoff=220,
-                                            strand=row.loc['strand']))
+        row_anno_df_list.append(reads.build_anno_df(row))
+        seq_mtx_list.append(reads.build_seq_array(window_offset))
+        m6a_mtx_list.append(reads.build_mod_array(window_offset,
+                                                  mod_type=M6A_MODS, sparse=True, 
+                                                  score_cutoff=220))
+        cpg_mtx_list.append(reads.build_mod_array(window_offset,
+                                                  mod_type=CPG_MODS, sparse=True, 
+                                                  score_cutoff=220))
     adata = ad.AnnData(
         obs=pd.concat(row_anno_df_list),
         var=pd.DataFrame({"pos" : np.arange(-window_offset, window_offset)})
@@ -285,15 +207,9 @@ def build_anndata_from_df(alignment_file, df, window_offset=1000):
     adata.layers['seq'] = np.vstack(seq_mtx_list)
     adata.layers['m6a'] = vstack(m6a_mtx_list).tocsr()
     adata.layers['cpg'] = vstack(cpg_mtx_list).tocsr()
-    # adata.obs = pd.concat(row_anno_df_list)
     # probably inefficient for large dfs
     adata.obs['site_name'] = ["{}:{}({})".format(row.seqid, row.pos, row.strand) 
                               for i, row in adata.obs.iterrows()]
-    # adata.obs_names = ["{} {}".format(obs_row.site_name, obs_row.read_name) 
-    #                    for i, obs_row in adata.obs.iterrows()]
-    # adata.var_names = np.arange(-window_offset, window_offset)
-    # adata.var = pd.DataFrame({"pos" : adata.var_names})
-    
     return(adata)
 
 def mark_cpg_sites(adata, sparse=True):
@@ -377,12 +293,10 @@ bamfile = pysam.AlignmentFile("local/aligned.fiberseq.chr3_trunc.bam", "rb")
 
 
 bed_data = read_bed('local/TAIR10_genes.bed')
-
-bed_data.query('not chrom in ["chrC", "chrM"]', inplace=True)
+# bed_data.query('not chrom in ["chrC", "chrM"]', inplace=True)
 
 
 anno_df = bed_to_anno_df(bed_data)
-
 anno_df.query('seqid == "chr3" & pos < 2000000', inplace=True) 
 
 
@@ -398,7 +312,7 @@ sys.getsizeof(fview)
 
 
 # -----------------------------------------------------------------------------
-
+# 
 
 
 
@@ -419,114 +333,25 @@ plot_data['bin'] = plot_data.pos // 10
 plot_data = plot_data.groupby('bin').sum()
 
 sns.relplot(data=plot_data, kind='line', 
-           x='bin', y='cpg_freq')
+           x='bin', y='m6a_freq')
 
 plt.show()
 
 
 # -----------------------------------------------------------------------------
-
-anno_df = pd.DataFrame({
-    "seqid" : ["chr3", "chr3", "chr3"],
-    "pos" : [2015001, 3000000, 3000000],
-    "strand" : ["+", "-", "+"],
-    "gene_id" : ["gene_1", "gene_2", "gene_3"],
-    "score" : [56, 600, 40]
-    })
-
-
-
-
 # check that cpgs are landing on Cs...
-fv2 = fview[fview.obs.gene_id == "gene_2"]
+
+fv2 = fview[fview.obs.gene_id == "AT3G01510"]
 
 seq_array = fv2.layers['seq'].copy()
 
 temp = seq_array[fv2.layers['cpg'].toarray()]
+for i in range(100):
+    print(temp[i])
 
+temp = seq_array[fv2.layers['m6a'].toarray()]
 for i in range(100):
     print(temp[i])
 
 
-
 # -----------------------------------------------------------------------------
-
-
-bed_data = read_bed('local/TAIR10_genes.bed')
-
-bed_data = bed_data.query('chrom == "chr3" & start < 2000000')
-
-anno_df = bed_to_anno_df(bed_data)
-
-anno_df = anno_df.iloc[500:,] # this will find some sites with no reads fast
-
-
-bed_data.query('not chrom in ["chrC", "chrM"]')
-
-# -----------------------------------------------------------------------------
-
-
-# reads = get_reads_at_center_pos(bamfile, GenomicPosition().from_str("chr3:2015001"))
-reads = get_reads_at_center_pos(bamfile, ("chr3", 2015001, '+'))
-
-
-reads = ReadList().get_reads(bamfile, ("chr3", 2015001, '+'))
-
-
-
-print_aligned_reads(reads, offset=50)
-
-
-
-# x = 50
-# mod_type = M6A_MODS
-# mods = get_strand_correct_mods(reads[x], mod_type)
-# print_mod_contexts(reads[x], mods, use_strand=False)
-# print(reads[x].alignment.is_reverse)
-
-win_offset = 2000
-filtered_reads = filter_reads_by_window(reads, win_offset)
-
-
-seq_array = build_seq_array(filtered_reads, win_offset)
-
-cpg_array = build_mod_array(filtered_reads, win_offset, mod_type=CPG_MODS, sparse=False)
-m6a_array = build_mod_array(filtered_reads, win_offset, mod_type=M6A_MODS)
-
-bytes(seq_array[0,:]).decode('UTF-8')[350]
-
-
-
-for i in np.arange(3000,3030):
-    I = m6a_array.row[i]
-    J = m6a_array.col[i]
-    seq = bytes(seq_array[I,:]).decode('UTF-8')
-    center_pos = J
-    offset = 10
-    print(str(i)+ ":  " + seq[center_pos-offset:center_pos] + ">" + 
-          seq[center_pos:center_pos+offset+1])
-
-# -----------------------------------------------------------------------------
-
-read = reads[5]
-temp = np.array(read.alignment.modified_bases_forward[CPG_MODS[0]])
-
-anno_df = pd.DataFrame({
-    "seqid" : ["chr3", "chr3", "chr3"],
-    "pos" : [2015001, 3000000, 4000000],
-    "strand" : ["+", "-", "-"],
-    "gene_id" : ["gene_1", "gene_2", "gene_3"],
-    "score" : [56, 600, 40]
-    })
-
-for i, row in anno_df.iterrows():
-    print(row)
-    row.loc['seqid']
-    type(row) == pd.core.series.Series
-
-row_df_dict = row.to_dict()
-vals = np.arange(9)
-row_df_dict['values'] = vals
-temp = build_row_anno_from_reads(reads, row)
-
-seq_array
