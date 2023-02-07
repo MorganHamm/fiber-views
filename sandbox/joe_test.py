bdc1142ae6243e421ba325c8eb8e4b21a3a2755f
Joseph Min <josephmin@gmail.com>
2023-02-06 16:26:34 -0800

    cluster by msp coverage and m6a count and make more plots

--- sandbox/joe_test.py
+++ sandbox/joe_test.py
@@ -39,20 +39,60 @@ all_positions = fv.FiberView(bamfile, anno_df, window=(-2000, 2000), fully_span=
 
 # Unique positions correspond with the individual loci (e.g. the ~900 loci for ONSEN LTR)
 pos_s = pd.unique(all_positions.obs.pos)
-single_position_view = all_positions[all_positions.obs.pos == pos_s[0]]
-plot = fv.tools.simple_region_plot(single_position_view)
+te_family_view = all_positions[all_positions.obs.pos == pos_s[0]]
+plot = fv.tools.simple_region_plot(te_family_view)
 fig = plot.get_figure()
 fig.savefig(f'{output_dir}/{family_name}{ending}.svg') # For saving in different formats: https://www.marsja.se/how-to-save-a-seaborn-plot-as-a-file-e-g-png-pdf-eps-tiff/
 
-# fv_filt = fv.tools.filter_regions(temp_view, 'msp', (100, np.inf))
+# Collapse down by site (i.e. "seqid:pos (strand)")
 obs_group_var = 'site_name'
-binned_by_position_view = fv.tools.agg_by_obs_and_bin(single_position_view, obs_group_var=obs_group_var, bin_width=20,
+binned_by_position_view = fv.tools.agg_by_obs_and_bin(te_family_view, obs_group_var=obs_group_var, bin_width=20,
                                           obs_to_keep=all_positions.obs.columns)
-plot = fv.tools.simple_region_plot(binned_by_position_view)
-fig = plot.get_figure()
-fig.savefig(f'{output_dir}/{family_name}{ending}_binned.svg')
-print(binned_by_position_view.head)
 
-# clust_feats = fv.tools.make_dense_regions(fv_filt, 'msp')
-# clust_feats = fv.tools.make_dense_regions(temp_view, 'msp')
-# clust_feats = temp_binned.layers['msp_coverage']
+cluster_features = binned_by_position_view.layers['msp_coverage']
+cluster_features = np.concatenate([binned_by_position_view.layers['m6a_count']], axis=1)
+
+pca = PCA(n_components=None, whiten=False)
+pca.fit(cluster_features)
+transformed = pca.transform(clust_feats)
+te_family_view.obsm['PCA'] = transformed
+
+# plot of variance explained by each PC
+plot = sns.scatterplot(x=np.arange(pca.components_.shape[0]), y=pca.explained_variance_ratio_)
+fig = plot.get_figure()
+fig.savefig(f'{output_dir}/{family_name}{ending}_PC_scatter.svg')
+
+# copy some PCs to obs for plotting
+te_family_view.obs['PC1'] = te_family_view.obsm['PCA'][:,0]
+te_family_view.obs['PC2'] = te_family_view.obsm['PCA'][:,1]
+te_family_view.obs['PC3'] = te_family_view.obsm['PCA'][:,2]
+
+# k-means clustering
+kmeans = KMeans(n_clusters=8, random_state=None, n_init=20).fit(te_family_view.obsm['PCA'][:,0:40])
+te_family_view.obs['km_clust'] = kmeans.labels_
+plot = sns.scatterplot(data=te_family_view.obs, x='PC1', y='PC2', hue='km_clust', palette="tab10")
+fig = plot.get_figure()
+fig.savefig(f'{output_dir}/{family_name}{ending}_kmeans_scatter.svg')
+# sns.scatterplot(data=temp_view.obs, x='PC3', y='PC2', hue='kmer_cluster', palette="tab10")
+
+# re-order and plot
+te_family_view_sorted = te_family_view[te_family_view.obs.sort_values(by='km_clust').index]
+te_family_view_sorted = fv_filt[te_family_view.obs.sort_values(by='km_clust').index] # show the filtered view
+plot = fv.tools.simple_region_plot(te_family_view_sorted)
+fig = plot.get_figure()
+fig.savefig(f'{output_dir}/{family_name}{ending}_sorted_by_cluster.svg')
+
+# aggregate each cluster, and plot a heatmap of MSP coverage
+te_family_view_sorted = te_family_view[te_family_view.obs.sort_values(by='km_clust').index]
+te_family_view_sorted.obs['clust'] = te_family_view_sorted.obs['km_clust'].astype(str)
+clustered_aggregated_view = fv.tools.agg_by_obs_and_bin(te_family_view_sorted, obs_group_var='clust', bin_width=10,
+                            obs_to_keep=['seqid', 'pos', 'strand', 'gene_id', 'score'])
+plot = sns.heatmap(clustered_aggregated_view.layers['msp_coverage'] / clustered_aggregated_view.layers['read_coverage'])
+fig = plot.get_figure()
+fig.savefig(f'{output_dir}/{family_name}{ending}_sorted_by_cluster_and_aggregated.svg')
+
+
+# plot the component loadings of the first 10 PCs
+plot = sns.heatmap(pca.components_[0:10, ], cmap="PiYG", center=0)
+fig = plot.get_figure()
+fig.savefig(f'{output_dir}/{family_name}{ending}_10_PCs.svg')
