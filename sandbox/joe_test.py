94542ab372d4f348758646189671a7a8a3125311
Joseph Min <josephmin@gmail.com>
2023-02-10 10:49:10 -0800

    close figures so they don't overlay

--- sandbox/joe_test.py
+++ sandbox/joe_test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import os
 import time
@@ -33,39 +34,50 @@ family_name = 'ATCOPIA78'
 endings = ['LTR']
 ending = endings[0]
 anno_df = pd.read_csv(f'/net/gs/vol1/home/joemin/te_extractions/{family_name}/{family_name}{ending}_msa.csv')
-anno_df.query('pos < 10000', inplace=True)
+anno_df.query('pos < 20000', inplace=True)
 
 all_positions = fv.FiberView(bamfile, anno_df, window=(-2000, 2000), fully_span=True)
 
 ### Unique positions correspond with the individual loci (e.g. the ~900 loci for ONSEN LTR)
 pos_s = pd.unique(all_positions.obs.pos)
-te_family_view = all_positions[all_positions.obs.pos == pos_s[0]]
+te_family_view = all_positions
+# te_family_view = all_positions[all_positions.obs.pos == pos_s[0]]
+print('Generating initial simple plot.')
 plot = fv.tools.simple_region_plot(te_family_view)
 fig = plot.get_figure()
 fig.savefig(f'{output_dir}/{family_name}{ending}.svg') # For saving in different formats: https://www.marsja.se/how-to-save-a-seaborn-plot-as-a-file-e-g-png-pdf-eps-tiff/
+fig.savefig(f'{output_dir}/{family_name}{ending}.png')
+plt.close(fig)
+
 
 ### Collapse down by site (i.e. "seqid:pos (strand)")
 obs_group_var = 'site_name'
 binned_view = fv.tools.agg_by_obs_and_bin(te_family_view, obs_group_var=obs_group_var, bin_width=20,
                                           obs_to_keep=all_positions.obs.columns)
 
-plot_binned = sns.heatmap(clustered_aggregated_view.layers['read_coverage'])
-fig_binned = plot_binned.get_figure()
-fig_binned.savefig(f'{output_dir}/{family_name}{ending}_binned.svg')
+print('Generating binned plot.')
+plot = sns.heatmap(binned_view.layers['nuc_coverage'])
+fig = plot.get_figure()
+fig.savefig(f'{output_dir}/{family_name}{ending}_binned.svg')
+fig.savefig(f'{output_dir}/{family_name}{ending}_binned.png')
+plt.close(fig)
 
 
 ### PCA
 cluster_features = binned_view.layers['msp_coverage']
-cluster_features = np.concatenate([binned_view.layers['m6a_count']], axis=1)
+# cluster_features = np.concatenate([binned_view.layers['m6a_count']], axis=1)
 
 pca = PCA(n_components=None, whiten=False)
 pca.fit(cluster_features)
 transformed = pca.transform(cluster_features)
 binned_view.obsm['PCA'] = transformed
 
+print('Generating scatter plot.')
 plot = sns.scatterplot(x=np.arange(pca.components_.shape[0]), y=pca.explained_variance_ratio_)
 fig = plot.get_figure()
 fig.savefig(f'{output_dir}/{family_name}{ending}_PC_scatter.svg')
+fig.savefig(f'{output_dir}/{family_name}{ending}_PC_scatter.png')
+plt.close(fig)
 
 binned_view.obs['PC1'] = binned_view.obsm['PCA'][:,0]
 binned_view.obs['PC2'] = binned_view.obsm['PCA'][:,1]
@@ -78,6 +90,8 @@ binned_view.obs['km_clust'] = kmeans.labels_
 plot = sns.scatterplot(data=binned_view.obs, x='PC1', y='PC2', hue='km_clust', palette="tab10")
 fig = plot.get_figure()
 fig.savefig(f'{output_dir}/{family_name}{ending}_kmeans_scatter.svg')
+fig.savefig(f'{output_dir}/{family_name}{ending}_kmeans_scatter.png')
+plt.close(fig)
 
 
 ### re-order and plot
@@ -86,6 +100,7 @@ binned_view_sorted = fv_filt[binned_view.obs.sort_values(by='km_clust').index] #
 plot = fv.tools.simple_region_plot(binned_view_sorted)
 fig = plot.get_figure()
 fig.savefig(f'{output_dir}/{family_name}{ending}_sorted_by_cluster.svg')
+fig.savefig(f'{output_dir}/{family_name}{ending}_sorted_by_cluster.png')
 
 
 ### aggregate each cluster, and plot a heatmap of MSP coverage
@@ -96,9 +111,11 @@ clustered_aggregated_view = fv.tools.agg_by_obs_and_bin(binned_view_sorted, obs_
 plot = sns.heatmap(clustered_aggregated_view.layers['msp_coverage'] / clustered_aggregated_view.layers['read_coverage'])
 fig = plot.get_figure()
 fig.savefig(f'{output_dir}/{family_name}{ending}_sorted_by_cluster_and_aggregated.svg')
+fig.savefig(f'{output_dir}/{family_name}{ending}_sorted_by_cluster_and_aggregated.png')
 
 
 ### plot the component loadings of the first 10 PCs
 plot = sns.heatmap(pca.components_[0:10, ], cmap="PiYG", center=0)
 fig = plot.get_figure()
 fig.savefig(f'{output_dir}/{family_name}{ending}_10_PCs.svg')
+fig.savefig(f'{output_dir}/{family_name}{ending}_10_PCs.png')
